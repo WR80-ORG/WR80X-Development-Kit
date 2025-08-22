532d64f442d77a74464f9d25557ec1d0e51190f6
FrancisBFTC <bftcorporations@gmail.com>
2025-08-22 15:57:50 -0300

    feat: Add strcpy and strncpy ASM implementation

---
+++ WDK1.0.0/Libraries/EXAMPLES/strcpy_ex.asm
@@ -0,0 +1,52 @@
+jp main
+
+include "string.inc"
+
+main:
+	clr
+	
+	st Str1::8
+	ld r0
+	st Str1::4
+	shl 4
+	st Str1::0
+	ld r1
+	
+	cdr
+	st Buf::8
+	ld r2
+	st Buf::4
+	shl 4
+	st Buf::0
+	ld r3
+	
+	; ---
+	push r3
+	push r2
+	push r1
+	push r0
+	call strcpy
+	; ---
+	
+	; Uncomment below for strncpy call and comment above between the '---' line
+	; cdr
+	; st 7
+	; push r3
+	; push r2
+	; push r1
+	; push r0
+	; pushd
+	; call strncpy
+	
+	; See R0 and DR in the bank register
+	; See also the logisim hexa editor in 0xB2 address
+	
+end:
+	jp end
+	
+Str1:
+	db "string copied",0
+
+Buf:
+	db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+	
