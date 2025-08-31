0d6f547727b33e9cfc5edff8a725e97c41583659
FrancisBFTC <bftcorporations@gmail.com>
2025-08-31 19:34:32 -0300

    feat: Recompila Emulador pra Windows

--- WDK1.1.0/Emulators/WR80EMU/wr80emu_private.h
+++ WDK1.1.0/Emulators/WR80EMU/wr80emu_private.h
@@ -5,19 +5,19 @@
 #define WR80EMU_PRIVATE_H
 
 /* VERSION DEFINITIONS */
-#define VER_STRING	"1.0.0.224"
+#define VER_STRING	"1.0.2.225"
 #define VER_MAJOR	1
 #define VER_MINOR	0
-#define VER_RELEASE	0
-#define VER_BUILD	224
+#define VER_RELEASE	2
+#define VER_BUILD	225
 #define COMPANY_NAME	"Wenderson Anjos (KiddieOS.Community)"
-#define FILE_VERSION	"1.0.0.224"
+#define FILE_VERSION	"1.0.2.225"
 #define FILE_DESCRIPTION	"WR80 Emulator for Windows CLI"
 #define INTERNAL_NAME	""
 #define LEGAL_COPYRIGHT	""
 #define LEGAL_TRADEMARKS	""
 #define ORIGINAL_FILENAME	""
 #define PRODUCT_NAME	""
-#define PRODUCT_VERSION	"1.0.0.224"
+#define PRODUCT_VERSION	"1.0.2.225"
 
 #endif /*WR80EMU_PRIVATE_H*/
